861b834cb3661642612db2cbb8bcec0c9e0e7bf3
Adam Jelenszky <jelenszkyadamzoltan@gmail.com>
2026-02-06 13:51:05 +0100

    fix testMatchers

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,6 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -53,12 +52,14 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-      testMatch: ['**/api/**'],
+      testDir: 'tests/api',
+      // testMatch: ['**/tests/api/**'],
     },
 
     {
       name: 'chromium-ui',
       use: { ...devices['Desktop Chrome'], storageState: 'cookies/.cookies/cookies.json' },
+      testDir: 'tests',
       testMatch: ['**/ui/**', '**/hybrid/**'],
       dependencies: ['setup'],
     },
